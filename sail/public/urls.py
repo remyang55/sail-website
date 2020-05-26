da6f14a6d15c9d58ec6e931ff9e3363d80bf0ead
Rem Yang <remyang2@illinois.edu>
2020-05-25 23:35:52 -0500

    Add navbar and basic about page

--- sail/public/urls.py
+++ sail/public/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='sail-home'),
+    path('about/', views.about, name='sail-about'),
 ]
--- sail/public/views.py
+++ sail/public/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'public/home.html')
 
+def about(request):
+    return render(request, 'public/about.html', {'title':'About'})
