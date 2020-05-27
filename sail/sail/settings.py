5c1b496e5282b47eb98573f009e87ae3827bb7a3
Rem Yang <remyang2@illinois.edu>
2020-05-27 15:54:46 -0500

    Define custom User model

--- sail/sail/settings.py
+++ sail/sail/settings.py
@@ -31,7 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'public.apps.PublicConfig',
+    
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom settings
+
+AUTH_USER_MODEL = 'users.SailUser'
