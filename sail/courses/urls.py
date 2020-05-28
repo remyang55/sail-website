6c7b081e8f91758b6ffda9ec8a2553efbe76cbc8
Rem Yang <remyang2@illinois.edu>
2020-05-28 18:33:04 -0500

    Create course detail view

--- sail/courses/urls.py
+++ sail/courses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CourseListView
+from .views import (CourseListView, 
+                    CourseDetailView,
+)
 from . import views
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='courses_list'),
+    path('<int:pk>/', CourseDetailView.as_view(), name='courses_detail'),
 ]
--- sail/courses/views.py
+++ sail/courses/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (ListView,
+                                  DetailView,
+)
 from .models import Course
 
 class CourseListView(ListView):
     model = Course
     context_object_name = 'courses'
+
+class CourseDetailView(DetailView):
+    model = Course
+    context_object_name = 'course'
