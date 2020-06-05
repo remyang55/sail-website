a2f4a932fbee468898f543f161c4d1052dc1b1b6
Rem Yang <remyang2@illinois.edu>
2020-06-04 19:42:49 -0500

    Create admin action to approve courses

--- sail/courses/admin.py
+++ sail/courses/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Course, Room, Section
 
-admin.site.register(Course)
 admin.site.register(Room)
 admin.site.register(Section)
+
+def approve_course(modeladmin, request, queryset):
+    queryset.update(approved=True)
+approve_course.short_description = 'Approve course'
+
+class CourseAdmin(admin.ModelAdmin):
+    actions = [approve_course, ]
+
+admin.site.register(Course, CourseAdmin)
