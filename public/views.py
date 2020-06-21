b126114f5a471204b6de061317c6135ec92f1d75
Rem Yang <remyang2@illinois.edu>
2020-06-21 03:40:16 -0500

    Only display footer for certain pages

--- public/views.py
+++ public/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'public/home.html')
+    return render(request, 'public/home.html', {'display_footer':True})
 
 def about(request):
-    return render(request, 'public/about.html', {'title':'About'})
+    return render(request, 'public/about.html', {'title':'About', 'display_footer':True})
 
 def staff(request):
-    return render(request, 'public/staff.html', {'title':'Staff'})
+    return render(request, 'public/staff.html', {'title':'Staff', 'display_footer':True})
